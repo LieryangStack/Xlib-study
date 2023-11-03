871d8c0bd18c6d55c7cfd44497ad5241bc43f8ae
LieryangStack <SXlisibo@gmail.com>
2023-11-03 22:31:14 +0800

    update

--- BasicGraphicsProgramming/01_simple_window.c
+++ BasicGraphicsProgramming/01_simple_window.c
@@ -1,5 +1,5 @@
 /*************************************************************************************************************************************************
- * @filename: 01_simple_drawing.c
+ * @filename: 01_simple_window.c
  * @author: EryangLi
  * @version: 1.0
  * @date: Oct-31-2023
@@ -20,19 +20,24 @@
 #include <stdlib.h>		/* getenv(), etc. */
 #include <unistd.h>		/* sleep(), etc.  */
 
-int main(int argc, char* argv[])
-{
-  Display* display;		/* pointer to X Display structure.           */
-  int screen_num;		/* number of screen to place the window on.  */
-  Window win;			/* pointer to the newly created window.      */
-  unsigned int display_width,
-               display_height;	/* height and width of the X display.        */
-  unsigned int width, height;	/* height and width for the new window.      */
-  unsigned int win_x, win_y;	/* location of the window's top-left corner. */
-  unsigned int win_border_width; /* width of window's border.                */
-  char *display_name = getenv("DISPLAY");  /* address of the X display.      */
+int 
+main(int argc, char* argv[]) {
+  Display* display;		/* X Display结构体指针 */
+  int screen_num;		/* 屏幕序号，X11角度来看多屏幕与现在多屏幕是不同的  */
+  Window win;			/* 被创建的窗口ID */
+  unsigned int display_width, 
+               display_height;	/*  display显示屏的长和宽 */
+  unsigned int width, height;	/* win显示窗口的长和宽  */
+  unsigned int win_x, win_y;	/* win显示窗口的坐标*/
+  unsigned int win_border_width; /* win显示窗口边界宽度 */
+  char *display_name = getenv("DISPLAY");  /* 获取环境变量DISPLAY的值 */
 
-	/* 连接到X服务器 */
+	/**
+   * 1. display命名规范 hostname:display_number.screen_number
+   *    可以这样理解：一台计算机可以有多个 display，一个 display 可以有多个屏幕。
+   *    指定参数的时候：因为可以省略掉 hostname 和 screen_number，所以可以用 :0 、:1 、:2 等等
+   * 2. 这个函数用来连接X服务器，Ubuntu Gnome桌面的X服务器就是 Xorg（开机的时候，显示管理器会运行Xorg）
+  */
   display = XOpenDisplay(display_name);
   if (display == NULL) {
     fprintf(stderr, "%s: cannot connect to X server '%s'\n",
@@ -40,36 +45,36 @@ int main(int argc, char* argv[])
     exit(1);
   }
 
-  /* get the geometry of the default screen for our display. */
+  /* 得到显示屏幕序号，一般screen_num都是0 */
   screen_num = DefaultScreen(display);
+  
+  /* 显示屏长宽 */
   display_width = DisplayWidth(display, screen_num);
   display_height = DisplayHeight(display, screen_num);
 
-  /* make the new window occupy 1/9 of the screen's size. */
+  /* 新建窗口大小是显示屏大小的三分之一*/
   width = (display_width / 3);
   height = (display_height / 3);
 
-  /* the window should be placed at the top-left corner of the screen. */
+  /* 放到屏幕左上角 */
   win_x = 0;
   win_y = 0;
 
-  /* the window's border shall be 2 pixels wide. */
+  /* 1. 新建窗口的边界为2个像素，但是由于窗口管理器的存在，这个边界是看不见的
+   * 2. 只有命令行模式下，只启动 Xorg（X服务器），才可以看见边界像素
+   */
   win_border_width = 2;
 
-  /* create a simple window, as a direct child of the screen's   */
-  /* root window. Use the screen's white color as the background */
-  /* color of the window. Place the new window's top-left corner */
-  /* at the given 'x,y' coordinates.                             */
+  /**
+   * 所有参数在头文件中都有注释，这里特别主要的是 RootWindow(display, screen_num)
+  */
   win = XCreateSimpleWindow(display, RootWindow(display, screen_num),
                             win_x, win_y, width, height, win_border_width,
                             BlackPixel(display, screen_num),
                             WhitePixel(display, screen_num));
 
-  /* make the window actually appear on the screen. */
+  
   XMapWindow(display, win); /* 缺少映射到窗口函数，则不会显示 */
-
-  /* flush all pending requests to the X server, and wait until */
-  /* they are processed by the X server.                        */
 
 	/* 1. 强制处理之前所有发送到服务器（display）的请求，并且等待所有事件处理完成才返回。
 	 * 2. discard: 是一个布尔值。如果设置为True，那么此函数还会清除所有尚未处理的事件。
@@ -87,7 +92,7 @@ int main(int argc, char* argv[])
 	XFlush(display);
 
   /* make a delay for a short period. */
-  while(1);
+  sleep(4);
 
   /* close the connection to the X server. */
   XCloseDisplay(display);
